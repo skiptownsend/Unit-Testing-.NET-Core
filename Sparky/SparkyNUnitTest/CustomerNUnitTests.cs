39c78dede728989994268a3991cfe47ab316ad32
Skip Townsend <skiptownsend@outlook.com>
2022-12-16 13:52:02 -0700

    V23 - Asserting String Values

---
+++ Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class CustomerNUnitTests
+    {
+        [Test]
+        public void GreetAndCombineNames_InputFirstAndLastName_ReturnFullName()
+        {
+            //Arrange
+            Customer customer = new();
+            
+            //Act
+            string fullName = customer.GreetAndCombineNames("Ben", "Spark");
+
+            //Assert
+            Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
+        }
+    }
+}
