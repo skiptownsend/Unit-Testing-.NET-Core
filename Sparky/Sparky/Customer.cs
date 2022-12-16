9ff47fa5e5934c15c83834c1fb7f5342a1218f4a
Skip Townsend <skiptownsend@outlook.com>
2022-12-16 16:51:49 -0700

    V33: Setup Inheritance

--- Sparky/Sparky/Customer.cs
+++ Sparky/Sparky/Customer.cs
@@ -9,6 +9,7 @@ namespace Sparky
     public class Customer
     {
         public int Discount = 15;
+        public int OrderTotal { get; set; }
         public string GreetMessage { get; set; }
         public string GreetAndCombineNames(string firstName, string lastName)
         {
@@ -21,5 +22,21 @@ namespace Sparky
             Discount = 20;
             return GreetMessage;
         }
+
+        public CustomerType GetCustomerDetails()
+        {
+            if (OrderTotal < 100)
+            {
+                return new BasicCustomer();
+            }
+            else
+            {
+                return new PlatinumCustomer();
+            }
+        }
     }
+
+    public class CustomerType { }
+    public class BasicCustomer : CustomerType { }
+    public class PlatinumCustomer : CustomerType { }
 }
