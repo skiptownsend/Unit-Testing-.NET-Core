3dd0be2c7688d0848ee8a6f8386f4832f7d7ff64
Skip Townsend <skiptownsend@outlook.com>
2022-12-19 11:59:34 -0700

    V67: Customer Xunit

--- Sparky/SparkyXUnit/CustomerXUnitTests.cs
+++ Sparky/SparkyXUnit/CustomerXUnitTests.cs
@@ -1,101 +1,98 @@
-﻿//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Sparky
-//{
-//    [TestFixture]
-//    public class CustomerXUnitTests
-//    {
-//        private Customer customer;
+namespace Sparky
+{
+    public class CustomerXUnitTests
+    {
+        private Customer customer;
+        public CustomerXUnitTests()
+        {
+            customer = new();
+        }
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            customer = new();
-//        }
+        [Fact]
+        public void GreetAndCombineNames_InputFirstAndLastName_ReturnFullName()
+        {
+            //Arrange
+            //Customer customer = new ();
 
-//        [Test]
-//        public void GreetAndCombineNames_InputFirstAndLastName_ReturnFullName()
-//        {
-//            //Arrange
-//            //Customer customer = new();
+            //Act
+            customer.GreetAndCombineNames("Ben", "Spark");
+
+            //Assert
+            Assert.Equal("Hello, Ben Spark", customer.GreetMessage);
+            Assert.Contains("ben Spark".ToLower(), customer.GreetMessage.ToLower());
+            Assert.StartsWith("Hello,", customer.GreetMessage);
+            Assert.EndsWith("Spark",customer.GreetMessage);
+            Assert.Matches("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", customer.GreetMessage);
+        }
+
+        [Fact]
+        public void GreetMessage_NotGreeted_ReturnsNull()
+        {
+            //Arrange
+            //Customer customer = new();
+
+            //Act
+
+            //Assert
+            Assert.Null(customer.GreetMessage);
+        }
+
+        [Fact]
+        public void DiscountCheck_DefaultCustomer_ReturnsDiscountInRange()
+        {
+            int result = customer.Discount;
+
+            Assert.InRange(result, 10, 25); //Test to confirm that the discount is only ever between 10 and 25 percent
+        }
+
+        [Fact]
+        public void GreetMessage_GreetedWithoutLastName_ReturnsNotNull()
+        {
+            customer.GreetAndCombineNames("ben", "");
+
+            Assert.NotNull(customer.GreetMessage);
+            Assert.False(string.IsNullOrEmpty(customer.GreetMessage));
+        }
+
+        [Fact]
+        public void GreetChecker_EmptyFirstName_ThrowsException()
+        {
+            var exceptionDetails = Assert.Throws<ArgumentException>(() => customer.GreetAndCombineNames("", "Spark"));
+            Assert.Equal("Empty First Name", exceptionDetails.Message);
+
+            //Next two lines not available in Xunit
+            //Assert.That(() => customer.GreetAndCombineNames("", "spark"),
+            //    Throws.ArgumentException.With.Message.EqualTo("Empty First Name"));
+
+            //Test whether exception is thrown without worrying about what the exception string is
+            Assert.Throws<ArgumentException>(() => customer.GreetAndCombineNames("", "Spark"));
             
-//            //Act
-//            customer.GreetAndCombineNames("Ben", "Spark");
+            //Next two lines not available in Xunit
+            //Assert.That(() => customer.GreetAndCombineNames("", "spark"),
+            //    Throws.ArgumentException);
+        }
 
-//            //Assert
-//            Assert.Multiple(() => //Assert.Multiple allows you to run an entire block of tests, even if one of them fails. Without it, if a test fails, any remaining tests will not run
-//            {
-//                Assert.AreEqual(customer.GreetMessage, "Hello, Ben Spark");
-//                Assert.That(customer.GreetMessage, Is.EqualTo("Hello, Ben Spark"));
-//                Assert.That(customer.GreetMessage, Does.Contain("ben Spark").IgnoreCase);
-//                Assert.That(customer.GreetMessage, Does.StartWith("Hello,"));
-//                Assert.That(customer.GreetMessage, Does.EndWith("Spark"));
-//                Assert.That(customer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
-//            });
-//        }
+        [Fact]
+        public void CustomerType_CreateCustomerWithLessThan100Order_ReturnBasicCustomer()
+        {
+            customer.OrderTotal = 10;
+            var result = customer.GetCustomerDetails();
+            Assert.IsType<BasicCustomer>(result);
+        }
 
-//        [Test]
-//        public void GreetMessage_NotGreeted_ReturnsNull()
-//        {
-//            //Arrange
-//            //Customer customer = new();
-
-//            //Act
-
-//            //Assert
-//            Assert.IsNull(customer.GreetMessage);
-//        }
-
-//        [Test]
-//        public void DiscountCheck_DefaultCustomer_ReturnsDiscountInRange()
-//        {
-//            int result = customer.Discount;
-
-//            Assert.That(result, Is.InRange(10, 25)); //Test to confirm that the discount is only ever between 10 and 25 percent
-//        }
-
-//        [Test]
-//        public void GreetMessage_GreetedWithoutLastName_ReturnsNotNull()
-//        {
-//            customer.GreetAndCombineNames("ben", "");
-
-//            Assert.IsNotNull(customer.GreetMessage);
-//            Assert.IsFalse(string.IsNullOrEmpty(customer.GreetMessage));
-//        }
-
-//        [Test]
-//        public void GreetChecker_EmptyFirstName_ThrowsException()
-//        {
-//            var exceptionDetails = Assert.Throws<ArgumentException>(() => customer.GreetAndCombineNames("", "Spark"));
-//            Assert.AreEqual("Empty First Name", exceptionDetails.Message);
-//            Assert.That(() => customer.GreetAndCombineNames("", "spark"),
-//                Throws.ArgumentException.With.Message.EqualTo("Empty First Name"));
-
-//            //Test whether exception is thrown without worrying about what the exception string is
-//            Assert.Throws<ArgumentException>(() => customer.GreetAndCombineNames("", "Spark"));
-//            Assert.That(() => customer.GreetAndCombineNames("", "spark"),
-//                Throws.ArgumentException);
-//        }
-
-//        [Test]
-//        public void CustomerType_CreateCustomerWithLessThan100Order_ReturnBasicCustomer()
-//        {
-//            customer.OrderTotal = 10;
-//            var result = customer.GetCustomerDetails();
-//            Assert.That(result, Is.TypeOf<BasicCustomer>());
-//        }
-
-//        [Test]
-//        public void CustomerType_CreateCustomerWithMoreThan100Order_ReturnPlatinumCustomer()
-//        {
-//            customer.OrderTotal = 200;
-//            var result = customer.GetCustomerDetails();
-//            Assert.That(result, Is.TypeOf<PlatinumCustomer>());
-//        }
-//    }
-//}
+        [Fact]
+        public void CustomerType_CreateCustomerWithMoreThan100Order_ReturnPlatinumCustomer()
+        {
+            customer.OrderTotal = 200;
+            var result = customer.GetCustomerDetails();
+            Assert.IsType<PlatinumCustomer>(result);
+        }
+    }
+}
