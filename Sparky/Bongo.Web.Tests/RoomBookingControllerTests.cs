ba7256dd1f3576691f449613827f2f4b503942bc
Skip Townsend <skiptownsend@outlook.com>
2022-12-20 22:42:33 -0700

    V85: Bongo Web Index Action Method Unit Test

---
+++ Sparky/Bongo.Web.Tests/RoomBookingControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Bongo.Core.Services.IServices;
+using Bongo.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.Web
+{
+    [TestFixture]
+    public class RoomBookingControllerTests
+    {
+        private Mock<IStudyRoomBookingService> _studyRoomBookingService;
+        private RoomBookingController _bookingController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _studyRoomBookingService = new Mock<IStudyRoomBookingService>();
+            _bookingController = new RoomBookingController(_studyRoomBookingService.Object);
+        }
+
+        [Test]
+        public void IndexPage_CallRequest_VeriftyGetAlInvoked()
+        {
+            _bookingController.Index();
+            _studyRoomBookingService.Verify(x => x.GetAllBooking(), Times.Once);
+        }
+    }
+}
