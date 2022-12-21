6c3321be2e4085c130165745a496c2dddf43dcdf
Skip Townsend <skiptownsend@outlook.com>
2022-12-20 23:06:55 -0700

    V87: Bongo Web Controller Unit Tests

--- Sparky/Bongo.Web.Tests/RoomBookingControllerTests.cs
+++ Sparky/Bongo.Web.Tests/RoomBookingControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Bongo.Core.Services.IServices;
 using Bongo.Models.Model;
+using Bongo.Models.Model.VM;
 using Bongo.Web.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -40,6 +41,47 @@ namespace Bongo.Web
 
             ViewResult viewResult = result as ViewResult;
             Assert.AreEqual("Book", viewResult.ViewName);
+        }
+
+        [Test]
+        public void BookRoomCheck_NotSuccessful_NoRoomCode()
+        {
+            _studyRoomBookingService.Setup(x => x.BookStudyRoom(It.IsAny<StudyRoomBooking>()))
+                .Returns(new StudyRoomBookingResult()
+                 {
+                     Code = StudyRoomBookingCode.NoRoomAvailable
+                 });
+            
+            var result = _bookingController.Book(new StudyRoomBooking());
+            Assert.IsInstanceOf<ViewResult>(result);
+            ViewResult viewResult = result as ViewResult;
+            Assert.AreEqual("No Study Room available for selected date", viewResult.ViewData["Error"]);
+        }
+
+        [Test]
+        public void BookRoomCheck_Successful_SuccessCodeAndRedirect()
+        {
+            _studyRoomBookingService.Setup(x => x.BookStudyRoom(It.IsAny<StudyRoomBooking>()))
+                .Returns((StudyRoomBooking booking) => new StudyRoomBookingResult()
+                {
+                    Code = StudyRoomBookingCode.Success,
+                    FirstName = booking.FirstName,
+                    LastName = booking.LastName,
+                    Date = booking.Date,
+                    Email = booking.Email
+                });
+
+            var result = _bookingController.Book(new StudyRoomBooking() { 
+                Date = DateTime.Now,
+                Email = "todd@gmail.com",
+                FirstName = "Todd",
+                LastName = "Sparky",
+                StudyRoomId = 1
+            });
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+            RedirectToActionResult actionResult = result as RedirectToActionResult;
+            Assert.AreEqual("Todd", actionResult.RouteValues["FirstName"]);
+            Assert.AreEqual(StudyRoomBookingCode.Success, actionResult.RouteValues["Code"]);
         }
     }
 }
